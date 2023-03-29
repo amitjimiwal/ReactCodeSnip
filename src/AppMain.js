b2ac470be605be433be2338d3373db3f0df9d7a0
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-03-29 11:29:00 +0530

    Updated Routes

--- src/AppMain.js
+++ src/AppMain.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import App2 from "./App2";
 import App from "./App";
 import Navbar from "./components/Navbar";
+import App3 from "./App3";
 
 const AppMain = () => {
   return (
@@ -12,8 +13,9 @@ const AppMain = () => {
       <Navbar/>
         <Routes>
           <Route path="/" />
-          <Route path="/firstpage" element={<App2 />} />
-          <Route path="/secondpage" element={<App />} />
+          <Route path="firstpage" element={<App2 />} />
+          <Route path="secondpage" element={<App />} />
+          <Route path="thirdpage/*" element={<App3 />} />
         </Routes>
       </Router>
     </div>
