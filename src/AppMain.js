859cfb9ca61f31b151cbfcb4218794a78fa30b97
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-04-04 15:47:03 +0530

    Useref Hook covered

--- src/AppMain.js
+++ src/AppMain.js
@@ -1,25 +1,12 @@
-import React from "react";
 import style from "./App.module.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import App2 from "./App2";
-import App from "./App";
-import Navbar from "./components/Navbar";
-import App3 from "./App3";
-import App4 from "./App4";
+import React from "react";
+import Hookuseref from "./components/Hookuseref";
 
 const AppMain = () => {
   return (
     <div className={style.App}>
-      <Router>
-      <Navbar/>
-        <Routes>
-          <Route path="/" />
-          <Route path="firstpage" element={<App2 />} />
-          <Route path="secondpage" element={<App />} />
-          <Route path="thirdpage/*" element={<App3 />} />
-          <Route path="/fourthpage" element={<App4 />} />
-        </Routes>
-      </Router>
+    <Hookuseref/>
+
     </div>
   );
 };
