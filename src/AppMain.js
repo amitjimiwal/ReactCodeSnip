fcf37614d75836011a765570b3e25c57d470f295
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-05-02 23:56:38 +0530

    lazy loading react components

--- src/AppMain.js
+++ src/AppMain.js
@@ -1,12 +1,20 @@
 import style from "./App.module.css";
-import React from "react";
-import Hookuseref from "./components/Hookuseref";
-
+import React ,{lazy,Suspense}from "react";
+import Home from "./components/Home";
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+const Hookuseref=lazy(()=> import("./components/Hookuseref"));
 const AppMain = () => {
   return (
     <div className={style.App}>
-    <Hookuseref/>
-
+    <Router>
+     <Suspense>
+     <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="hooksuseref" element={<Hookuseref/>}/>
+      </Routes>
+     </Suspense>
+    </Router>
+    
     </div>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import { useContext } from 'react'
-import { App3Context } from '../App3'
-import { useQuery } from '@tanstack/react-query'
+// import { useContext } from 'react'
+// import { App3Context } from '../App3'
+// import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 const Home = () => {
-  const {username}=useContext(App3Context);
-  const {data}=useQuery(["cat"],()=>{
-     return axios.get("https://catfact.ninja/fact").then(res =>res.data)
-  })
+  // const {username}=useContext(App3Context);
+  // const {data}=useQuery(["cat"],()=>{
+  //    return axios.get("https://catfact.ninja/fact").then(res =>res.data)
+  // })
   return (
-    <div>This is home page of {username}
-    <p>{data?.fact}</p></div>
+    <div>This is home page
+      <Link to="hooksuseref">YoyO</Link></div>
+  
   )
 }
 
