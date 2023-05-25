a6f2e63eef7acf0a1f056df09f1ced03290603fb
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-05-25 18:00:39 +0530

    first commit of memo branch

--- src/AppMain.js
+++ src/AppMain.js
@@ -1,20 +1,7 @@
 import style from "./App.module.css";
-import React ,{lazy,Suspense}from "react";
-import Home from "./components/Home";
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
-const Hookuseref=lazy(()=> import("./components/Hookuseref"));
 const AppMain = () => {
   return (
     <div className={style.App}>
-    <Router>
-     <Suspense>
-     <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="hooksuseref" element={<Hookuseref/>}/>
-      </Routes>
-     </Suspense>
-    </Router>
-    
     </div>
   );
 };
